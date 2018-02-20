a0adf5b0fe0898908bfda0dbd93a2b886e7a125a
Ubuntu <test@mail>
2018-02-20 19:20:33 +0000

    User GET commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 from flask import jsonify
 import json
 import sqlite3
@@ -21,6 +20,27 @@ def home_index():
 		api_list.append(a_dict)
 	conn.close()    
 	return jsonify({'api_version': api_list}), 200
+
+def list_users():
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor = conn.execute("SELECT username, full_name, emailid, password, id from users")
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['name'] = row[1]
+        a_dict['email'] = row[2] 
+        a_dict['password'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+    conn.close()
+    return jsonify({'user_list': api_list})
+
+
+@app.route('/api/v1/users', methods=['GET']) 
+def get_users():
+    return list_users()
   
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
